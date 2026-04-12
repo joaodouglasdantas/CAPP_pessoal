3bfce066d42aac0f10b31325c2df64fdf6311fef
joaodouglasdantas <joaodouglasdantas02b@gmail.com>
2026-04-12 15:32:23 -0300

    fix: corrigindo exclusao em cascata

--- app/models/unidade.rb
+++ app/models/unidade.rb
@@ -2,6 +2,7 @@ class Unidade < ApplicationRecord
   belongs_to :bloco
   has_many :moradores_unidades, dependent: :destroy
   has_many :moradores, through: :moradores_unidades, source: :usuario
+  has_many :chamados, dependent: :destroy
 
   validates :numero_andar, presence: true
   validates :numero_unidade, presence: true
